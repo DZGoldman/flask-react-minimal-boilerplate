de116f1b511cd2a46fcf30099bc519b5b1555745
Daniel Goldman <dzgoldman@wesleyan.edu>
2018-11-02 11:45:10 -0400

    add routes, stuff for heroku deploy, dependencies, and backend data check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      backEndData: "data NOT received (yet?)"
+    };
+
+    this.dataUrl = "/data";
+    if (process.env.NODE_ENV == "development") {
+      this.dataUrl = "http://0.0.0.0:33507" + this.dataUrl;
+    };
+
+  }
+  
+  componentDidMount = () =>{
+    axios.get(this.dataUrl).then(d => {
+      this.setState({backEndData :d.data})
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +31,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            {this.state.backEndData}
           </p>
           <a
             className="App-link"
